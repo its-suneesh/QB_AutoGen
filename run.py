dc8ec6420665c4871fa60130dc9e81c87079dd01
muhammedashiqkm <ashiq.kottakuthm@gmail.com>
2025-07-29 15:43:19 +0530

    Update run.py

--- run.py
+++ run.py
@@ -6,4 +6,4 @@ app = create_app()
 
 if __name__ == "__main__":
     # Use Gunicorn or another WSGI server in production
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
